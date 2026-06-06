d6a7a06aa2eb4548921da7ac554d8df7fa6cdc1b
DAT-cy <dat224k@gmail.com>
2026-06-06 10:13:37 +0000

    feat : add site config

--- helpdesk/config/config_sites.py
+++ helpdesk/config/config_sites.py
@@ -1,13 +1,31 @@
 import frappe
 
 def get_nextgrp_develop_config():
-    """Lấy thông tin cấu hình của site develop.nextgrp.vn từ site_config.json"""
-    api_key = frappe.conf.get("api_key")
-    api_secret = frappe.conf.get("api_secret")
-    base_url = frappe.conf.get("site_url")
+    """Lấy thông tin cấu hình từ HD Site Config"""
+    
+    organization = frappe.conf.get("organization")
+    
+    config = frappe.get_all(
+        "HD Site Config",
+        filters={"organization": organization},
+        fields=["name", "site_url", "api_key"],
+        limit=1
+    )
+    
+    if not config:
+        frappe.throw("Chưa cấu hình thông tin trong HD Site Config")
+        
+    config_doc = config[0]
+    api_key = config_doc.get("api_key")
+    base_url = config_doc.get("site_url")
+    
+    try:
+        api_secret = frappe.utils.password.get_decrypted_password("HD Site Config", config_doc.name, "api_secret")
+    except Exception:
+        api_secret = None
     
     if not api_key or not api_secret:
-        frappe.throw("Chưa cấu hình api_key_develop hoặc api_secret_develop trong site_config.json")
+        frappe.throw("Chưa cấu hình api_key hoặc api_secret trong HD Site Config")
         
     return {
         "url": base_url,
